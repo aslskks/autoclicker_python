9e1a28ec8927e868c809b6d6108fcb41fe82a82a
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-21 15:29:10 -0600

    CODE

--- autoclicker.py
+++ autoclicker.py
@@ -1,8 +1,41 @@
-import pyautogui as py
-import keyboard
+import tkinter as tk
+import pyautogui
+import threading
 
-while True:
-    if keyboard.read_key() == "shift":
-        for i in range(10):
-            posicion = (py.position())
-            py.click(posicion)
+class AutoClickerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Auto Clicker")
+
+        # Etiqueta
+        self.label = tk.Label(root, text="Auto Clicker")
+        self.label.pack(pady=10)
+
+        # Botón para activar/desactivar
+        self.button = tk.Button(root, text="Activa perro", command=self.toggle_auto_click)
+        self.button.pack()
+
+        # Variable para rastrear el estado del auto clicker
+        self.auto_clicking = False
+
+    def toggle_auto_click(self):
+        if not self.auto_clicking:
+            self.auto_clicking = True
+            self.button.config(text="Desactivar")
+            self.auto_click_thread = threading.Thread(target=self.auto_click)
+            self.auto_click_thread.start()
+        else:
+            self.auto_clicking = False
+            self.button.config(text="Activar")
+
+    def auto_click(self):
+        while self.auto_clicking:
+            posiosion = pyautogui.position()
+            pyautogui.click(posiosion)
+            # Puedes ajustar la velocidad de clic aquí
+            pyautogui.PAUSE = 100
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AutoClickerApp(root)
+    root.mainloop()
